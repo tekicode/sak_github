26367a097061a88692fc5a83a3bd596f83da3db5
teki-on-gate <teki@teki.sh>
2022-06-23 06:04:42 +0000

    clarity on vars, functions to cache, limit lookups based upon real accounts

--- sak_github.sh
+++ sak_github.sh
@@ -1,29 +1,49 @@
 #!/usr/bin/env bash
 PATH="/bin:/usr/bin"
 
-# Map a provided user to a github user
-case "$1" in
-	"tekicode")	gitkeys="tekicode";;
+age() { echo $(( $(/usr/bin/date +%s) - $(/usr/bin/date -r "$1" +%s) )); }
 
-	# Fallthrough
-	*)	gitkeys="tekicode";;
-esac
+fetchkeys() {
+	# Download the keyfile to a tmp file
+	/usr/bin/curl -s --fail --max-time 5.5 -o "${2}.tmp" "https://github.com/${1}.keys"
+	if [ $? -gt 0 ]; then
+		/bin/cat "$2" 2>/dev/null
+		exit $?
+	else
+		/bin/cat "${2}.tmp" && \
+		/bin/mv "${2}.tmp" "${2}" && \
+		/usr/bin/logger -t sak_github -p info "Fetched keys for user [$1] to keyfile [$2]" && \
+		exit 0
 
-# Silent curl, suppres errors on failure, no more than 5 seconds to fetch, save in /var/tmp
-/usr/bin/curl -s --fail --max-time 5 -o /var/tmp/${gitkeys}.authkeys.tmp https://github.com/${gitkeys}.keys
-ret=$?
+		/usr/bin/logger -t sak_github -p error "Failed to fetch keys for user [$1] to keyfile [$2]."
+		exit 1
+	fi
+}
 
-# If ret is somehow unset, we'll fail by default.
-if [ ${ret:-1} -gt 0 ]; then
-	# Try to dump this file if it exists or not.
-	/bin/cat /var/tmp/${gitkeys}.authkeys 2>/dev/null
-	# If it existed, we'll exit 0, otherwise >0
-	exit $?
-else
-	# dump the downloaded tmp file, then copy it to the primary authkeys file
-	/bin/cat /var/tmp/${gitkeys}.authkeys.tmp && \
-	/bin/mv /var/tmp/${gitkeys}.authkeys.tmp /var/tmp/${gitkeys}.authkeys && \
-	/usr/bin/logger -t "sak_github" "Got keys for user [$1] using keysource github.com/${gitkeys}.keys"
+# Don't do checks on user accounts that don't exist locally
+if ! /usr/bin/id -u "$1" >/dev/null 2>&1; then
 	exit 0
 fi
+
+# Map a provided user to a github user
+case "$1" in
+	"root")	gituser="tekicode";;
+
+	# Catch-all
+	*) gituser="tekicode";;
+esac
+
+keyfile="/var/tmp/${gituser}.pubkeys"
+if [ -f "$keyfile" ]; then
+	ageist=$(age "$keyfile")
+	if [ "${ageist:-9999}" -gt 900 ]; then
+		/usr/bin/logger -t sak_github -p info "Refreshing pubkeys for user [$1] keys [$keyfile] age: [$ageist]"
+		fetchkeys "${gituser}" "$keyfile"
+	else
+		cat $keyfile 2>/dev/null
+		exit $?
+	fi
+fi
+
+fetchkeys "$gituser" "$keyfile"
 
